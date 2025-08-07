674d6753102ee8f3278060941738317294589240
amnashahid7 <naheedamna5@gmail.com>
2025-08-07 12:11:34 +0500

    var,let,const

---
+++ Lecture2/variables.js
@@ -0,0 +1,42 @@
+// var
+// let
+// const
+
+// const 
+const pie=3.14;
+let age=12;
+console.log(age);
+age=15;
+console.log(age);
+
+
+// let pie=3.14567;   // cause an error Identifier 'pie' has already been declared
+// var , let
+
+
+// block scope var (not block scope)
+
+
+
+// {
+//     let i=10;
+//     let name="amna";
+//     console.log(name,i);
+// }
+
+// // console.log(name);
+// let i=100;
+// console.log(i);
+
+var name = "amna";
+console.log(name,"first var");
+
+{
+      name="abdullah";
+    console.log(name,"inside from the block scope");
+    
+}
+
+// var name="abdul salam";
+console.log(name,"end of the code");
+
