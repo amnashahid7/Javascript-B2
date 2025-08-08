027418e2235806daa964d4168d243bd9a4e058af
amnashahid7 <naheedamna5@gmail.com>
2025-08-08 12:34:37 +0500

    conversions

---
+++ Lecture3/conversions.js
@@ -0,0 +1,27 @@
+let name= "amna";
+// console.log(typeof name);
+
+
+let convertedStr= Number(name);
+// console.log(typeof convertedStr);
+
+
+let num= 12;
+// console.log(typeof num);
+
+let convertedNum= String(num);
+// console.log(typeof convertedNum);
+
+
+let bool= false;
+console.log(typeof bool);
+
+let convertedBool= Number(bool);
+console.log(typeof convertedBool,convertedBool);
+
+
+
+
+
+
+
