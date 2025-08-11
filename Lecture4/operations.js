7b54d2b785ad68f5b6b07f8bd0fbd0b0e1069e48
amnashahid7 <naheedamna5@gmail.com>
2025-08-11 12:05:37 +0500

    operators

---
+++ Lecture4/operations.js
@@ -0,0 +1,74 @@
+// console.log("operations.js loaded");
+
+
+
+let positiveNumber=10;
+let convertedNumber= -positiveNumber;
+// console.log(convertedNumber, typeof convertedNumber);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+// console.log(2**3);
+
+
+let firstName="Amna";
+let lastName="Khan";
+//concatenation
+// console.log(firstName+" "+lastName);
+
+
+// console.log(1+"2");
+// console.log(1+"2"+"2");
+// console.log("1"+2+2);
+
+// console.log(2+2+"1"+2+2);
+// console.log(2+2+"1"+2+2-1);
+
+// console.log("2"*2)
+// console.log("2"*"2")
+// console.log("2"/2)
+// console.log("2"/"2")
+// console.log("2"-"2")
+
+
+// console.log(+true,"+true");
+// console.log(-true,"-true");
+// console.log(true+true); 
+// console.log(+false,"+false");
+// console.log(-false,"-false");
+// console.log(false+false); 
+
+
+
+// console.log(+"heelo","+heelo");
+// console.log(+"2","+2");
+
+// wrong practice
+// let num1,num2,num3;
+// num1=num2=num3=5;
+num1=5
+num2=5
+num3=5;
+console.log(num2);
+
+// HomeWork
+// postfix, prefix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
