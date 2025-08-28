fda94668d9f0e038797f03f316f765268be4150e
amnashahid7 <naheedamna5@gmail.com>
2025-08-28 15:20:40 +0500

    numbers and math

---
+++ Lecture6/numbers.js
@@ -0,0 +1,34 @@
+// console.log("numbers.js loaded");
+console.log("100"-"20")
+console.log("100"-"heelo")
+console.log("abv"-"heelo")
+console.log("100"*20);
+console.log("10"*"20");
+console.log("10"*"heelo");
+console.log("10"/"2");
+console.log("10"/2);
+console.log("10"+2);
+
+
+let number=10.5545;
+console.log(number.toExponential());
+console.log(number.toExponential(4));
+console.log(number.toFixed());
+console.log(number.toPrecision(1));
+console.log(typeof number.toLocaleString());
+console.log(number.toLocaleString());
+console.log(number.valueOf());
+
+console.log(Math);
+
+// sign , round,random,ceil,floor,min,max
+
+
+number=10.2758;
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+
+
+
+
+
