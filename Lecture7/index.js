15f5671e575450d53ca5d55a0e7ad631d77840f5
amnashahid7 <naheedamna5@gmail.com>
2025-08-29 15:47:54 +0500

    min max

---
+++ Lecture7/index.js
@@ -0,0 +1,24 @@
+// console.log(Math.min(1,2,3,-1,-5,0));
+// console.log(Math.max(1,2,3,-1,-5,0));
+
+
+// console.log(Math.random()+1);
+
+
+let min=10;
+let max=100;
+
+
+let randomNumber= Math.floor(Math.random()*(max-min)+min)
+
+// console.log(randomNumber);
+
+
+// HOMEWORK generate random number between 40 to 65
+
+
+console.log(Date());
+
+
+
+
