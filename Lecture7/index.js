00099b42768439d7da855c46791879e34bab7b17
amnashahid7 <naheedamna5@gmail.com>
2025-09-01 15:45:18 +0500

    date object

--- Lecture7/index.js
+++ Lecture7/index.js
@@ -17,7 +17,38 @@ let randomNumber= Math.floor(Math.random()*(max-min)+min)
 // HOMEWORK generate random number between 40 to 65
 
 
-console.log(Date());
+// console.log(Date());
+let date= new Date();
+// console.log(date)
+// console.log(date.toLocaleString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toString());
+// console.log(date.toISOString());
+// console.log(date.getFullYear());
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+
+
+// let createNewDate=new Date("2020-12-5");
+let createNewDate=new Date(2020,11,5);
+// console.log(createNewDate);
+// console.log(createNewDate.toLocaleString());
+// console.log(createNewDate.toString());
+
+
+let newDate=Date.now();
+console.log(newDate/24/60/60/1000); //days since 1970
+console.log(newDate/60/60/1000); //hours since 1970
+console.log(newDate/60/1000); //minutes since 1970
+console.log(newDate/1000); //seconds since 1970
+console.log(newDate); //milliseconds since 1970
+
+
+
 
 
 
