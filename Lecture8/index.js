863326f43d9b8a0faba11ace688a8e9621ce389b
amnashahid7 <naheedamna5@gmail.com>
2025-09-02 15:22:19 +0500

    array,push,pop,shift,unshift

---
+++ Lecture8/index.js
@@ -0,0 +1,25 @@
+let arr1=[1,2,3,4,5,6,7];
+let arr2=["abdullah","jalil","abdul-salam"];
+let arr3=["amna",78, true,{},[5,67.5,"amna"]];
+
+
+// access the array value
+console.log(arr1[5]);
+console.log(arr3[4][1]);
+
+
+arr1.push(8);
+arr1.push(80);
+console.log(arr1);
+arr1.pop()
+console.log(arr1);
+
+arr1.unshift("amna")
+
+console.log(arr1);
+arr1.shift();
+console.log(arr1);
+
+
+
+
