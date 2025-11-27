541973ab03c86cff2f9dfa86e1957384e2f2ecab
mr.sherry <mr.sherry6967@gmail.com>
2025-11-27 17:10:13 +0500

    useeffect and fetch data

--- react-vite/condition/src/App.jsx
+++ react-vite/condition/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import Conditions from './Components/Conditions';
 import Forms from './Components/Forms';
+import SideEffects from './Components/SideEffects';
 
 function App() {
+  
   // create form with five fields and show data of that fileds using conditional redring with loading
   // create two seperate componenets one form and second ui of cards and pss data in props
 
   return (
     <div>
-      <Forms />
+      <SideEffects  />
     </div>
   );
 }
---
+++ react-vite/condition/src/Components/SideEffects.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+function SideEffects() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      console.log('timeout run');
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [data]);
+
+  async function getData() {
+    try {
+      let res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      let data = await res.json();
+      setData(data);
+      console.log('data fetched success');
+    } catch (error) {
+      console.log('got error', error);
+    }
+    }
+    
+    // https://jsonplaceholder.typicode.com/
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          let num = Math.random();
+          setData(`hello ${num}`);
+        }}
+      >
+        hello
+      </button>
+      <div>hello </div>
+    </div>
+  );
+}
+
+export default SideEffects;
