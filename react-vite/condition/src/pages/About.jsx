14f3f2622fcc3dc19528ac0bdbf7a624cb0410fd
mr.sherry <mr.sherry6967@gmail.com>
2025-12-30 17:39:07 +0500

    remaining hooks basics

--- react-vite/condition/src/pages/About.jsx
+++ react-vite/condition/src/pages/About.jsx
@@ -1,20 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useId, useRef } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
 function About() {
-  const { theme } = useContext(ThemeContext);
+  // const { theme } = useContext(ThemeContext);
+
+  const id = useId();
+  const id2 = useId();
+
+  let inputRef = useRef(45678);
+
+  useEffect(() => {
+    console.log(inputRef.current);
+
+    inputRef.current = 29292;
+    console.log(inputRef.current);
+  }, []);
 
   return (
     <div>
-      About
-      <div
-        style={{
-          backgroundColor: theme,
-          border: '2px solid black',
-          height: '200px',
-          width: '200px',
-        }}
-      ></div>
+      <form action=''>
+        <label htmlFor={id}>password</label>
+        <input type='password' name='' id={id} />
+        <h1>hello register</h1>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
